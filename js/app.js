3c3a0085a24853e8e46d2fcd255f8ce1d097b013
damien-d13 <devoti.damien@gmail.com>
2021-08-24 11:49:11 +0200

    Add pacman image on Tray

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 console.log("This is PACMAN");
-var pacman = document.getElementById("pacman");
+var pacmanGame = document.getElementById("pacman");
+
+var pacmanImage = new Image();
+pacmanImage.src = "img/pacman4.gif"
+
 
 // Table tiles map size 19/22
 var imgArray = new Array();
@@ -28,40 +32,52 @@ var imgArray = new Array();
     [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
-// Initila information of pacman
-var pacman = {
-    x : 1,
+// Initialise information of pacman
+let pacmanPosition = {
+    x : 11,
     y : 1,
-    direction : 0
+    direction : 2.0
 };
         
         console.log(imgArray);
 
 // Execute the code when the page is already load.
 window.addEventListener("load", (event) => {
-
+function DisplayTray(){
     // Add image to Array cell    
     for (let i = 0; i < imgArray.length; i++) {
         for (let j = 0; j < imgArray[i].length; j++) {
             if (imgArray[i][j] === 0) {
                 imgArray[i][j] = new Image();
+                imgArray[i][j].style.gridArea = (i + 1) + " / " + (j + 1);
                 imgArray[i][j].src = "img/mur.gif";
-                pacman.appendChild(imgArray[i][j]);
+                pacmanGame.appendChild(imgArray[i][j]);
             }else if (imgArray[i][j] === 1){
                 imgArray[i][j] = new Image();
+                imgArray[i][j].style.gridArea = (i + 1) + " / " + (j + 1);
                 imgArray[i][j].src = "img/sol.gif";
-                pacman.appendChild(imgArray[i][j]);
+                pacmanGame.appendChild(imgArray[i][j]);
             } else if (imgArray[i][j] === 2) {
                 imgArray[i][j] = new Image();
+                imgArray[i][j].style.gridArea = (i + 1) + " / " + (j + 1);
                 imgArray[i][j].src = "img/bonbon.gif";
-                pacman.appendChild(imgArray[i][j]);
+                pacmanGame.appendChild(imgArray[i][j]);
             }
             
             
         }
         
     }
-
+};
+DisplayTray();
 console.log(imgArray);
-   
+   function GameSequence(){
+        
+   };
+
+   function displayPacman(){
+        pacmanImage.style.gridArea = pacmanPosition.x + " / " + pacmanPosition.y;
+        pacmanGame.appendChild(pacmanImage);
+    };
+    displayPacman();
 });
