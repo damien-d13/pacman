4785114418c990bdeff5ac92b002881add04edd0
Damien Devoti <devoti.damien@gmail.com>
2021-08-26 17:02:19 +0200

    Add key event to main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Execute the code when the page is already load.
 window.addEventListener("load", (event) => {
+    
     var gridTiles = new GridTiles(document.getElementById("pacman"));
     var pacman = new Pacman(document.getElementById("pacman")); 
     gridTiles.displayTiles();
@@ -14,4 +15,28 @@ window.addEventListener("load", (event) => {
     }
     var intervalGame = setInterval(gameSequence, 200);
 
+    //add keyboard keys
+  function keyPackman(key) {
+    // alert(key.code);
+    // console.log(pacman.direction);
+    switch (key.code) {
+      case "ArrowUp":
+        pacman.direction = 2;
+        break;
+      case "ArrowDown":
+        pacman.direction = 1;
+        break;
+      case "ArrowLeft":
+        pacman.direction = 4;
+        break;
+      case "ArrowRight":
+        pacman.direction = 3;
+        break;
+
+      default:
+        break;
+    };
+  };
+
+document.body.addEventListener("keydown", keyPackman);
 });
